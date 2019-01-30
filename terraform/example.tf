a0c272f315bb27881f8bcc676bb23f4323f5dcf9
Karl Azzam <karlazzam@yahoo.com>
2019-01-29 22:38:36 -0500

    removing old example tf

--- terraform/example.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-}
-
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-}
